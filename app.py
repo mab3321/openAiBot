fa8cffba4688d91510b8436f4488c401b059853f
mab <mab@mab.com>
2023-08-30 17:45:20 +0500

    Flask App Initialized

---
+++ app.py
@@ -0,0 +1,53 @@
+from index import ChatGptBotAPI
+from flask import Flask, request, jsonify
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+app = Flask(__name__)
+
+openai_api_key = config.get("OPENAI_API_KEY")
+bot = ChatGptBotAPI(openai_api_key)
+
+
+@app.route('/create', methods=['POST'])
+def create_prompt():
+    data = request.json
+    prompt = data.get('prompt')
+    bot.create_prompt(prompt)
+    return jsonify({"message": "Prompt created successfully"}), 201
+
+
+@app.route('/read', methods=['GET'])
+def read_prompt():
+    index = int(request.args.get('index', None))
+    print(index)
+    prompts = bot.read_prompt(index)
+    if prompts is None:
+        return jsonify({"error": "Invalid index"}), 400
+    return jsonify({"prompts": prompts}), 200
+
+
+@app.route('/update', methods=['PUT'])
+def update_prompt():
+    data = request.json
+    index = data.get('index')
+    new_prompt = data.get('new_prompt')
+    try:
+        bot.update_prompt(index, new_prompt)
+        return jsonify({"message": "Prompt updated successfully"}), 200
+    except IndexError:
+        return jsonify({"error": "Invalid index"}), 400
+
+
+@app.route('/delete', methods=['DELETE'])
+def delete_prompt():
+    data = request.json
+    index = data.get('index')
+    try:
+        bot.delete_prompt(index)
+        return jsonify({"message": "Prompt deleted successfully"}), 200
+    except IndexError:
+        return jsonify({"error": "Invalid index"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ index.py
@@ -0,0 +1,65 @@
+import openai
+import os
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+
+class ChatGptBotAPI:
+    def __init__(self, openaikey):
+
+        openai_api_key = config.get("OPENAI_API_KEY")
+        openai.api_key = openai_api_key
+        self.prompts = []
+
+    def get_response(self, index):
+        prompt = self.prompts[index]
+        response_from_api = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+        response_to_send = response_from_api.choices[0]['message']['content']
+        return response_to_send
+
+    def create_prompt(self, prompt):
+        self.prompts.append(prompt)
+
+    def read_prompt(self, index=None):
+        if index is None:
+            return self.prompts
+        if 0 <= index < len(self.prompts):
+            return self.prompts[index]
+        else:
+            return None
+
+    def update_prompt(self, index, new_message):
+        if 0 <= index < len(self.prompts):
+            self.prompts[index] = new_message
+        else:
+            raise IndexError("Index out of range")
+
+    def delete_prompt(self, index):
+        if 0 <= index < len(self.prompts):
+            del self.prompts[index]
+        else:
+            raise IndexError("Index out of range")
+
+
+# Usage
+openai_api_key = config.get("OPENAI_API_KEY")
+print(openai_api_key)
+bot = ChatGptBotAPI(openai_api_key)
+
+# Create messages
+bot.create_prompt("Hello there!")
+bot.create_prompt("How can I help you?")
+bot.create_prompt("Feel free to ask any questions.")
+
+# Read messages
+print("All messages:", bot.read_prompt())
+print("Message at index 1:", bot.read_prompt(1))
+
+# Update message
+bot.update_prompt(0, "Greetings!")
+print("Updated messages:", bot.read_prompt())
+
+# Delete message
+bot.delete_prompt(2)
+print("Remaining messages:", bot.read_prompt())
